5eca365c980fb534d0b18c1843583b9794804f48
Lucas Lima <57077913+astatonn@users.noreply.github.com>
2022-01-10 22:25:49 -0300

    finishing portifolio

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 <html lang="pt-BR">
 
 <head>
+    <!-- METADADOS -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Solicite um orçamento para mixagem e masterização de músicas com um dos melhores engenheiros de áudio do mercado">
+
+    <!-- LINKS EXTERNOS -->
+    <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://allfont.net/allfont.css?fonts=agency-fb" rel="stylesheet" type="text/css" />
+
+    <!-- TÍTULO E FAVICON -->
     <title>Leandro Santos - Engenheiro de Áudio</title>
 </head>
 
@@ -96,9 +102,11 @@
         <section class="conteudo-portifolio">
             <h2 class="conteudo-subtitulo">Últimos trabalhos</h2>
             <p class="conteudo-subhead-texto">Ouça os últimos trabalhos nas principais streams de áudio do mercado</p>
-            <div class="ultimos-trabalhos">
-                <div class="conteudo-ultimo-trabalho">
-                    <div class="musica">
+            <div class="carrossel-trabalhos">
+                
+                <!-- MUNDO REAL -->
+                <div class="ultimos-trabalhos">
+                    <div class="conteudo-ultimo-trabalho">        
                         <img src="assets/img/portifolio/mundo_real.jpeg" alt="ícone de mixagem de som" class="portifolio-imagem">
                         <h4 class="musica-titulo">Arthur Pares - Mundo Real</h4>
                         <ul>
@@ -133,10 +141,210 @@
                                 </a>
                             </li>
                         </ul>
+                    </div>
+                </div>
 
+
+                    <div class="ultimos-trabalhos">
+                        <div class="conteudo-ultimo-trabalho">
+                            <!-- MUNDO REAL -->
+                            <img src="assets/img/portifolio/mundo_real.jpeg" alt="ícone de mixagem de som" class="portifolio-imagem">
+                            <h4 class="musica-titulo">Arthur Pares - Mundo Real</h4>
+                            <ul>
+                                <li>
+                                    <a href="#" class="musica-item spotify">
+                                        <span class="texto-stream">Spotify</span>
+                                        <img src="assets/img/icones/spotify.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item deezer">
+                                        <span class="texto-stream">Deezer</span>
+                                        <img src="assets/img/icones/deezer.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item tidal">
+                                        <span class="texto-stream">Tidal</span>
+                                        <img src="assets/img/icones/tidal.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item apple">
+                                        <span class="texto-stream">iTunes</span>
+                                        <img src="assets/img/icones/itunes.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item youtube">
+                                        <span class="texto-stream">YouTube</span>
+                                        <img src="assets/img/icones/youtube.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        </div>
+                        
+                    <div class="ultimos-trabalhos">
+                        <div class="conteudo-ultimo-trabalho">
+                            <!-- MUNDO REAL -->
+                            <img src="assets/img/portifolio/mundo_real.jpeg" alt="ícone de mixagem de som" class="portifolio-imagem">
+                            <h4 class="musica-titulo">Arthur Pares - Mundo Real</h4>
+                            <ul>
+                                <li>
+                                    <a href="#" class="musica-item spotify">
+                                        <span class="texto-stream">Spotify</span>
+                                        <img src="assets/img/icones/spotify.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item deezer">
+                                        <span class="texto-stream">Deezer</span>
+                                        <img src="assets/img/icones/deezer.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item tidal">
+                                        <span class="texto-stream">Tidal</span>
+                                        <img src="assets/img/icones/tidal.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item apple">
+                                        <span class="texto-stream">iTunes</span>
+                                        <img src="assets/img/icones/itunes.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item youtube">
+                                        <span class="texto-stream">YouTube</span>
+                                        <img src="assets/img/icones/youtube.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        </div>
+                        <!-- MUNDO REAL -->
+                <div class="ultimos-trabalhos">
+                    <div class="conteudo-ultimo-trabalho">        
+                        <img src="assets/img/portifolio/mundo_real.jpeg" alt="ícone de mixagem de som" class="portifolio-imagem">
+                        <h4 class="musica-titulo">Arthur Pares - Mundo Real</h4>
+                        <ul>
+                            <li>
+                                <a href="#" class="musica-item spotify">
+                                    <span class="texto-stream">Spotify</span>
+                                    <img src="assets/img/icones/spotify.svg" alt="ícone do youtube" class="icone-stream">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="musica-item deezer">
+                                    <span class="texto-stream">Deezer</span>
+                                    <img src="assets/img/icones/deezer.svg" alt="ícone do youtube" class="icone-stream">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="musica-item tidal">
+                                    <span class="texto-stream">Tidal</span>
+                                    <img src="assets/img/icones/tidal.svg" alt="ícone do youtube" class="icone-stream">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="musica-item apple">
+                                    <span class="texto-stream">iTunes</span>
+                                    <img src="assets/img/icones/itunes.svg" alt="ícone do youtube" class="icone-stream">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="musica-item youtube">
+                                    <span class="texto-stream">YouTube</span>
+                                    <img src="assets/img/icones/youtube.svg" alt="ícone do youtube" class="icone-stream">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                     </div>
 
-                </div>
+                    <div class="ultimos-trabalhos">
+                        <div class="conteudo-ultimo-trabalho">
+                            <!-- MUNDO REAL -->
+                            <img src="assets/img/portifolio/mundo_real.jpeg" alt="ícone de mixagem de som" class="portifolio-imagem">
+                            <h4 class="musica-titulo">Arthur Pares - Mundo Real</h4>
+                            <ul>
+                                <li>
+                                    <a href="#" class="musica-item spotify">
+                                        <span class="texto-stream">Spotify</span>
+                                        <img src="assets/img/icones/spotify.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item deezer">
+                                        <span class="texto-stream">Deezer</span>
+                                        <img src="assets/img/icones/deezer.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item tidal">
+                                        <span class="texto-stream">Tidal</span>
+                                        <img src="assets/img/icones/tidal.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item apple">
+                                        <span class="texto-stream">iTunes</span>
+                                        <img src="assets/img/icones/itunes.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item youtube">
+                                        <span class="texto-stream">YouTube</span>
+                                        <img src="assets/img/icones/youtube.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        </div>
+                    <div class="ultimos-trabalhos">
+                        <div class="conteudo-ultimo-trabalho">
+                            <!-- MUNDO REAL -->
+                            <img src="assets/img/portifolio/mundo_real.jpeg" alt="ícone de mixagem de som" class="portifolio-imagem">
+                            <h4 class="musica-titulo">Arthur Pares - Mundo Real</h4>
+                            <ul>
+                                <li>
+                                    <a href="#" class="musica-item spotify">
+                                        <span class="texto-stream">Spotify</span>
+                                        <img src="assets/img/icones/spotify.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item deezer">
+                                        <span class="texto-stream">Deezer</span>
+                                        <img src="assets/img/icones/deezer.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item tidal">
+                                        <span class="texto-stream">Tidal</span>
+                                        <img src="assets/img/icones/tidal.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item apple">
+                                        <span class="texto-stream">iTunes</span>
+                                        <img src="assets/img/icones/itunes.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="musica-item youtube">
+                                        <span class="texto-stream">YouTube</span>
+                                        <img src="assets/img/icones/youtube.svg" alt="ícone do youtube" class="icone-stream">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- ./FIM CARROSSEL -->
+                    </div>
 
         </section>
 
@@ -144,7 +352,24 @@
         <section class="conteudo-sobre">
             <div class="conteudo-sobre-escrito">
                 <h2 class="conteudo-subtitulo">Sobre</h2>
-                <p class="conteudo-sobre-texto">Vida do Lenadro Santos</p>
+                <p class="conteudo-subhead-texto">Conheça mais sobre a vida do Leandro Santos</p>
+            </div>
+            <div class="sobre-conteudo">
+                <div class="conteudo-texto">
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. 
+                        It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+                        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+                        looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+                        and going through the cites of the word in classical literature, discovered the undoubtable source. 
+                        Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" 
+                        (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory 
+                        of ethics, very popular during the Renaissance. 
+                        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
+                        Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced 
+                        in their exact original form, accompanied by English versions from the 1914 
+                        translation by H. Rackham</p>
+                </div>
             </div>
             <!-- <img class="conteudo-sobre-imagem" src="http://learnenglish.britishcouncil.org/sites/podcasts/files/2021-10/RS6715_492969113-hig.jpg" alt="Foto de LEandro Santos"> -->
         </section>
@@ -180,5 +405,6 @@
     </footer>
 
 </body>
+
 
 </html>
